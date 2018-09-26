2c68f9ff41380d4bc340ed4387a356546ffa7459
Sarah Herzog <235290+CodingDino@users.noreply.github.com>
2018-09-26 13:49:09 +0100

    Added GetSoundBuffer to AssetManager

--- AssetManager.cpp
+++ AssetManager.cpp
@@ -63,3 +63,23 @@ sf::Texture& AssetManager::GetTexture(std::string _fileName)
 	}
 
 }
+
+
+// See texture function above for notes
+sf::SoundBuffer& AssetManager::GetSoundBuffer(std::string _fileName)
+{
+	auto keyValuePair = s_instance->m_soundBuffers.find(_fileName);
+
+	if (keyValuePair != s_instance->m_soundBuffers.end())
+	{
+		// We DID find it
+		return keyValuePair->second;
+	}
+	else
+	{
+		// We did NOT find it
+		sf::SoundBuffer& soundBuffer = s_instance->m_soundBuffers[_fileName];
+		soundBuffer.loadFromFile(_fileName);
+		return soundBuffer;
+	}
+}
