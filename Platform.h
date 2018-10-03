c7e34d63374c3142755cf797b557978c0198964b
Sarah Herzog <235290+CodingDino@users.noreply.github.com>
2018-10-03 15:26:53 +0100

    Added platform (no collision)

---
+++ Platform.h
@@ -0,0 +1,22 @@
+#pragma once
+
+// Library Include
+#include <SFML/Graphics.hpp>
+
+class Platform
+{
+
+public:
+
+	Platform();
+
+	void Spawn();
+
+	void Draw(sf::RenderTarget& _target);
+
+private:
+
+	// Assets
+	sf::Sprite m_sprite;
+
+};
